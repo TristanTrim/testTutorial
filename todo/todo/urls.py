6a32599d6386e13d31430e69bba2b93b87cde01f
tristantrim <tristantrim@gmail.com>
2015-05-20 16:16:33 -0300

    first unit test, url map, and dummy view.

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -5,8 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'todo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+     url(r'^$', 'lists.views.home_page', name='home'),
+    #url(r'^admin/', include(admin.site.urls)),
 )
